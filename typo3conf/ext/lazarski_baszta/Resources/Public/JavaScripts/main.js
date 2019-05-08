1ff000d047c5909ae351f5bfd5a7881aabcf1592
Chris <swidrak@gmail.com>
2019-05-08 13:08:00 +0200

    c

--- typo3conf/ext/lazarski_baszta/Resources/Public/JavaScripts/main.js
+++ typo3conf/ext/lazarski_baszta/Resources/Public/JavaScripts/main.js
@@ -4,24 +4,37 @@ $(document).ready(function() {
         });
 
         var player1 = videojs('#my-player1');
+        player1.ready(function () {
+        	player1.usingNativeControls = true;
+        }
         // can also be `tap` or `touchend` or some other event instead of `click`
-        player1.on('touchend', function() {
-            if (player1.paused()) {
-                player1.play();
-            } else {
-                player1.pause();
-            }
+        player1.on('touchend',  function (e) {
+	        if (e.target.nodeName === 'VIDEO') {
+	            if (player1.paused()) {
+	                player1.play();
+	            } else {
+	                player1.pause();
+	            }
+	        }
         });
         var player2 = videojs('#my-player2');
+        player2.ready(function () {
+        	player2.usingNativeControls = true;
+        }
         // can also be `tap` or `touchend` or some other event instead of `click`
-        player2.on('touchend', function() {
-            if (player2.paused()) {
-                player2.play();
-            } else {
-                player2.pause();
-            }
+        player2.on('touchend',  function (e) {
+	        if (e.target.nodeName === 'VIDEO') {
+	            if (player2.paused()) {
+	                player2.play();
+	            } else {
+	                player2.pause();
+	            }
+	        }
         });
         var player3 = videojs('#my-player3');
+        player3.ready(function () {
+        	player3.usingNativeControls = true;
+    	}
         // can also be `tap` or `touchend` or some other event instead of `click`
         player3.on('touchend', function() {
             if (player3.paused()) {
@@ -31,6 +44,9 @@ $(document).ready(function() {
             }
         });
         var player4 = videojs('#my-player4');
+        player4.ready(function () {
+        	player4.usingNativeControls = true;
+    	}
         // can also be `tap` or `touchend` or some other event instead of `click`
         player4.on('touchend', function() {
             if (player4.paused()) {
